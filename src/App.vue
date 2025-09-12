03f26999e34c2ac4f0a8ce4cca0358061d0f07a8
Syphax LAKHDARCHAOUCHE <81050260+syphaxlch@users.noreply.github.com>
2025-09-12 00:01:14 -0400

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
-<script setup>
 
+<script setup>
 import Menu from "@/components/Menu.vue";
 import Liste_CV from "@/components/Liste_CV.vue";
 import Question from "@/components/Question.vue";
@@ -7,24 +7,21 @@ import {useCvStore} from "@/components/Store";
 import Export from "@/components/Export.vue";
 
 const store = useCvStore();
-
-
 </script>
 
+
 <template>
-
-    <div class="wrapper">
-      <Menu></Menu>
-
-      <Liste_CV></Liste_CV>
-      <div id="Core">
-        <Question id="question"></Question>
-        <component id="cv" :is="store.displayCv"></component>
-      </div>
-      <div><Export></Export>
-
-      </div>
+  <div class="wrapper">
+    <Menu />
+    <Liste_CV />
+    <div id="Core">
+      <Question id="question" />
+      <component id="cv" :is="store.displayCv" />
     </div>
+    <div>
+      <Export />
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -36,7 +33,6 @@ const store = useCvStore();
 #question{
   display: flex;
   width: 40%;
-  //overflow: hidden;
 }
 
 #cv{
---
+++ src/components/Accueil.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="page">
+    <div class="txt">
+      <h1>Bienvenue à la page d'accueil</h1>
+      <p><strong>Construisez votre avenir professionnel en un clic avec notre outil de création de CV intuitif et efficace !</strong></p>
+      <br>
+      <button class="button" @click="$emit('start')">Commencer</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Accueil',
+}
+</script>
+
+<style scoped>
+.button {
+  padding: 10px 20px;
+  font-size: 1.2em;
+  background: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.button:hover {
+  background: #0056b3;
+}
+</style>
